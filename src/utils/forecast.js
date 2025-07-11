94c615811878634661294b22b4a966f778516448
Pavanstrome <pavankumar33254@gmail.com>
2025-07-11 16:03:34 +0530

    commited

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,13 @@
  const request = require('postman-request')
 const forecast = (latitude, longitude, callback) => {
      const URL = 'https://api.weatherstack.com/current?access_key=490e0b03524cb62c175345b0a64eb259&query=' + latitude + ','+ longitude + '&units=f'
-
+ 
      request({url:URL,json:true}, (error,{body}) => {
      if(error){
       callback("unable to connect the network.....!", undefined)
      }else if(body.error){
       callback("unable to find the location.....!", undefined)
-     }else{
+     }else{ 
       callback(undefined, {
         location: body.location.name,
         temparature : body.current.temperature,
